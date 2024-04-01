b53e8bb5cc0ec020ce6368b2a56abd32724764bb
Cong-Cong <dacongsama@live.com>
2024-04-01 19:53:06 +0800

    feat: collect plugin options

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # plugins-diff-rspack-vs-webpack
+
+List the status of webpack built-in plugins implemented in rspack.
--- index.ts
+++ index.ts
@@ -5,8 +5,48 @@ import ts from "typescript";
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+type Type = string;
+
+interface Option {
+  name: string;
+  type: Type;
+}
+
+interface Plugin {
+  name: string;
+  options?: Type | Option[];
+}
+
+function getPluginOptions(checker: ts.TypeChecker, type: ts.Type): undefined | Type | Option[] {
+  if (type.isClassOrInterface()) {
+    const properties = type.getProperties();
+    return properties.reduce<Option[]>((result, property) => {
+      const type = checker.getTypeOfSymbol(property);
+      result.push({
+        name: property.escapedName.toString(),
+        type: checker.typeToString(type),
+      });
+      return result;
+    }, []);
+  } else {
+    return checker.typeToString(type);
+  }
+}
+
+function getPluginOptionsOfClass(checker: ts.TypeChecker, node: ts.ClassDeclaration) {
+  const constructor = node.members.find(ts.isConstructorDeclaration);
+  if (constructor) {
+    const firstParameter = constructor.parameters[0];
+    if (firstParameter && firstParameter.type) {
+      const typeNode = firstParameter.type;
+      const type = checker.getTypeAtLocation(typeNode);
+      return getPluginOptions(checker, type);
+    }
+  }
+}
+
 function getWebpackPlugins() {
-  const plugins: string[] = [];
+  const plugins: Plugin[] = [];
   const entry = path.join(__dirname, "webpack.ts");
   const program = ts.createProgram(
     [entry],
@@ -15,6 +55,7 @@ function getWebpackPlugins() {
       module: ts.ModuleKind.CommonJS,
     }
   );
+  const checker = program.getTypeChecker();
   for (const sourceFile of program.getSourceFiles()) {
     if (sourceFile.fileName.includes("webpack")) {
       ts.forEachChild(sourceFile, visit);
@@ -22,10 +63,14 @@ function getWebpackPlugins() {
   }
   function visit(node: ts.Node) {
     if (ts.isClassDeclaration(node) && node.name && ts.isIdentifier(node.name)) {
-      const name = node.name.escapedText;
-      if (name?.endsWith("Plugin")) {
-        plugins.push(name);
+      const name = node.name.escapedText.toString();
+      if (name.endsWith("Plugin")) {
+        plugins.push({
+          name,
+          options: getPluginOptionsOfClass(checker, node),
+        });
       }
+      return;
     }
     ts.forEachChild(node, visit);
   }
@@ -34,7 +79,7 @@ function getWebpackPlugins() {
 const webpackPlugins = getWebpackPlugins();
 
 function getRspackPlugins() {
-  const plugins: string[] = [];
+  const plugins: Plugin[] = [];
   const entry = path.join(__dirname, "rspack.ts");
   const program = ts.createProgram(
     [entry],
@@ -43,6 +88,7 @@ function getRspackPlugins() {
       module: ts.ModuleKind.CommonJS,
     }
   );
+  const checker = program.getTypeChecker();
   for (const sourceFile of program.getSourceFiles()) {
     if (sourceFile.fileName.includes("@rspack/core")) {
       ts.forEachChild(sourceFile, visit);
@@ -64,9 +110,26 @@ function getRspackPlugins() {
       if (ts.isVariableDeclaration(declaration) && ts.isIdentifier(declaration.name)) {
         const name = declaration.name.escapedText.toString();
         if (name.endsWith("Plugin")) {
-          plugins.push(name);
+          const options = (function () {
+            if (declaration.type) {
+              if (ts.isTypeLiteralNode(declaration.type)) {
+                const constructor = declaration.type.members.find(member => ts.isConstructSignatureDeclaration(member)) as ts.ConstructSignatureDeclaration;
+                const firstParameter = constructor.parameters[0];
+                if (firstParameter && firstParameter.type) {
+                  const typeNode = firstParameter.type;
+                  const type = checker.getTypeAtLocation(typeNode);
+                  return getPluginOptions(checker, type);
+                }
+              }
+            }
+          })();
+          plugins.push({
+            name,
+            options
+          });
         }
       }
+      return;
     }
     // export declare class XxxPlugin extends RspackBuiltinPlugin {
     //   constructor(options: XxxPluginOptions);
@@ -78,8 +141,12 @@ function getRspackPlugins() {
     ) {
       const name = node.name.escapedText.toString();
       if (name.endsWith("Plugin")) {
-        plugins.push(name);
+        plugins.push({
+          name,
+          options: getPluginOptionsOfClass(checker, node),
+        });
       }
+      return;
     }
     ts.forEachChild(node, visit);
   }
@@ -88,9 +155,20 @@ function getRspackPlugins() {
 const rspackPlugins = getRspackPlugins();
 
 const data = webpackPlugins.reduce<{plugin: string, status: string}[]>((result, webpackPlugin) => {
-  const status = rspackPlugins.includes(webpackPlugin) ? "🟢 Implemented" : "🔴 Not implemented";
+  const status = (function() {
+    const rspackPlugin = rspackPlugins.find(rspackPlugin => webpackPlugin.name === rspackPlugin.name);
+    if (!rspackPlugin) {
+      return "🔴 Not implemented";
+    }
+    if (rspackPlugin.options === webpackPlugin.options) {
+      return "🟢 Fully implemented";
+    }
+    return "🟡 Missing";
+  })();
+  
+
   result.push({
-    plugin: webpackPlugin,
+    plugin: webpackPlugin.name,
     status,
   });
   return result;
